ac002b209501531a73df3d5e152629149857f4e4
Zizaco <zizaco@gmail.com>
2013-02-07 18:10:58 -0200

    Migration template

---
+++ src/views/generators/migration.blade.php
@@ -0,0 +1,43 @@
+{{ '<?php' }}
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRolesTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Creates the roles table
+        Schema::create('roles', function($table)
+        {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('permissions'); // Model's permissions array parsed as JSON
+            $table->timestamps();
+        });
+
+        // Creates the assigned_roles (Many-to-Many relation) table
+        Schema::create('assigned_roles', function($table)
+        {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('role_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('assigned_roles');
+        Schema::drop('roles');
+    }
+
+}
