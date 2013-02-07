f98733443ddd9d70fd53358b680aacfcff95f7de
Zizaco <zizaco@gmail.com>
2013-02-07 04:30:42 -0200

    Initial commit

---
+++ src/Zizaco/Entrust/EntrustServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Zizaco\Entrust;
+
+use Illuminate\Support\ServiceProvider;
+
+class EntrustServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('zizaco/entrust');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
