addeba2a4265000e6138d7d76fee3521360fe5ff
Travis Olbrich <travis.olbrich@outlook.com>
2013-05-25 02:46:16 -0400

    Although this method of permission handling is being phased out, this is still required for migration and seeding.

--- src/views/generators/migration.blade.php
+++ src/views/generators/migration.blade.php
@@ -16,6 +16,7 @@ class EntrustSetupTables extends Migration {
         {
             $table->increments('id');
             $table->string('name');
+            $table->string('permissions');
             $table->timestamps();
         });
 
