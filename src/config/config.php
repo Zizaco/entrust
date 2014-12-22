fe67c77178c0cb881d333be978184694ad708dfc
Ben Batschelet <ben@codeup.com>
2014-12-22 09:47:46 -0600

    Tabs to spaces in config, make comments match actual values

--- src/config/config.php
+++ src/config/config.php
@@ -2,66 +2,66 @@
 
 return array(
 
-	/*
-	|--------------------------------------------------------------------------
-	| Entrust Role Model
-	|--------------------------------------------------------------------------
-	|
-	| This is the Role model used by Entrust to create correct relations.  Update
-	| the role if it is in a different namespace.
-	|
-	*/
-	'role' => '\Role',
+    /*
+    |--------------------------------------------------------------------------
+    | Entrust Role Model
+    |--------------------------------------------------------------------------
+    |
+    | This is the Role model used by Entrust to create correct relations.  Update
+    | the role if it is in a different namespace.
+    |
+    */
+    'role' => '\Role',
 
-	/*
-	|--------------------------------------------------------------------------
-	| Entrust Roles Table
-	|--------------------------------------------------------------------------
-	|
-	| This is the Roles table used by Entrust to save roles to the database.
-	|
-	*/
-	'roles_table' => 'roles',
+    /*
+    |--------------------------------------------------------------------------
+    | Entrust Roles Table
+    |--------------------------------------------------------------------------
+    |
+    | This is the roles table used by Entrust to save roles to the database.
+    |
+    */
+    'roles_table' => 'roles',
 
-	/*
-	|--------------------------------------------------------------------------
-	| Entrust Permission Model
-	|--------------------------------------------------------------------------
-	|
-	| This is the Permission model used by Entrust to create correct relations.  Update
-	| the permission if it is in a different namespace.
-	|
-	*/
-	'permission' => '\Permission',
+    /*
+    |--------------------------------------------------------------------------
+    | Entrust Permission Model
+    |--------------------------------------------------------------------------
+    |
+    | This is the Permission model used by Entrust to create correct relations.  Update
+    | the permission if it is in a different namespace.
+    |
+    */
+    'permission' => '\Permission',
 
-	/*
-	|--------------------------------------------------------------------------
-	| Entrust Permissions Table
-	|--------------------------------------------------------------------------
-	|
-	| This is the Permissions table used by Entrust to save permissions to the database.
-	|
-	*/
-	'permissions_table' => 'permissions',
+    /*
+    |--------------------------------------------------------------------------
+    | Entrust Permissions Table
+    |--------------------------------------------------------------------------
+    |
+    | This is the permissions table used by Entrust to save permissions to the database.
+    |
+    */
+    'permissions_table' => 'permissions',
 
-	/*
-	|--------------------------------------------------------------------------
-	| Entrust permission_role Table
-	|--------------------------------------------------------------------------
-	|
-	| This is the permission_role table used by Entrust to save relationship between permissions and roles to the database.
-	|
-	*/
-	'permission_role_table' => 'permission_role',
+    /*
+    |--------------------------------------------------------------------------
+    | Entrust permission_role Table
+    |--------------------------------------------------------------------------
+    |
+    | This is the permission_role table used by Entrust to save relationship between permissions and roles to the database.
+    |
+    */
+    'permission_role_table' => 'permission_role',
 
-	/*
-	|--------------------------------------------------------------------------
-	| Entrust assigned_roles Table
-	|--------------------------------------------------------------------------
-	|
-	| This is the assigned_roles table used by Entrust to save assigned roles to the database.
-	|
-	*/
-	'role_user_table' => 'role_user',
+    /*
+    |--------------------------------------------------------------------------
+    | Entrust role_user_table Table
+    |--------------------------------------------------------------------------
+    |
+    | This is the role_user table used by Entrust to save assigned roles to the database.
+    |
+    */
+    'role_user_table' => 'role_user',
 
 );
