305916c40ed35dd6ebb81379db51ec582df19623
Andrew Elkins <andrewelkins@users.noreply.github.com>
2015-03-20 08:05:21 -0700

    Add namespace to config. Thanks @aglipanci

--- src/config/config.php
+++ src/config/config.php
@@ -19,7 +19,7 @@ return array(
     | the role if it is in a different namespace.
     |
     */
-    'role' => '\Role',
+    'role' => 'App\Role',
 
     /*
     |--------------------------------------------------------------------------
@@ -40,7 +40,7 @@ return array(
     | Update the permission if it is in a different namespace.
     |
     */
-    'permission' => '\Permission',
+    'permission' => 'App\Permission',
 
     /*
     |--------------------------------------------------------------------------
