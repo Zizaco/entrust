87af3cd5f7a3dfd0deb7bb979a122edb35ef2c88
Greg <greg.hello@gmail.com>
2015-06-10 15:48:34 -0500

    Added permission check against role
    Added a check to see true or false check against a role to see the permissions it has assigned to it.

--- src/Entrust/Traits/EntrustRoleTrait.php
+++ src/Entrust/Traits/EntrustRoleTrait.php
@@ -53,6 +53,42 @@ trait EntrustRoleTrait
             return true;
         });
     }
+    
+    /**
+     * Checks if the role has a permission by its name.
+     *
+     * @param string|array $name       Permission name or array of permission names.
+     * @param bool         $requireAll All permissions in the array are required.
+     *
+     * @return bool
+     */
+    public function hasPerm($name, $requireAll = false)
+    {
+        if (is_array($name)) {
+            foreach ($name as $permName) {
+                $hasPerm = $this->hasPerm($permName);
+
+                if ($hasPerm && !$requireAll) {
+                    return true;
+                } elseif (!$hasPerm && $requireAll) {
+                    return false;
+                }
+            }
+
+            // If we've made it this far and $requireAll is FALSE, then NONE of the permissions were found
+            // If we've made it this far and $requireAll is TRUE, then ALL of the permissions were found.
+            // Return the value of $requireAll;
+            return $requireAll;
+        } else {
+            foreach ($this->perms as $perm) {
+                if ($perm->name == $name) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
 
     /**
      * Save the inputted permissions.
@@ -91,7 +127,7 @@ trait EntrustRoleTrait
     }
 
     /**
-     * Detach permission form current role.
+     * Detach permission from current role.
      *
      * @param object|array $permission
      *
