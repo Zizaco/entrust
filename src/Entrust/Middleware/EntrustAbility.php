be28aa634c44551ae5187894c903094a8ce8baee
Andrew Elkins <bitbucket@andrewelkins.com>
2015-11-12 09:38:37 -0800

    Add missing files

---
+++ src/Entrust/Middleware/EntrustAbility.php
@@ -0,0 +1,46 @@
+<?php namespace Zizaco\Entrust\Middleware;
+
+/**
+ * This file is part of Entrust,
+ * a role & permission management solution for Laravel.
+ *
+ * @license MIT
+ * @package Zizaco\Entrust
+ */
+
+use Closure;
+use Illuminate\Contracts\Auth\Guard;
+
+class EntrustAbility
+{
+	protected $auth;
+
+	/**
+	 * Creates a new instance of the middleware.
+	 *
+	 * @param Guard $auth
+	 */
+	public function __construct(Guard $auth)
+	{
+		$this->auth = $auth;
+	}
+
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param \Illuminate\Http\Request $request
+	 * @param Closure $next
+	 * @param $roles
+	 * @param $permissions
+	 * @param bool $validateAll
+	 * @return mixed
+	 */
+	public function handle($request, Closure $next, $roles, $permissions, $validateAll = false)
+	{
+		if ($this->auth->guest() || !$request->user()->ability(explode('|', $roles), explode('|', $permissions), array('validate_all' => $validateAll))) {
+			abort(403);
+		}
+
+		return $next($request);
+	}
+}
