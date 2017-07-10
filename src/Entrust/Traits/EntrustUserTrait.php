b55b7e350802bce5e08dc729b6410a908bc018b6
zlegend-210 <yamen.a.abdulrahman@hotmail.com>
2017-07-10 13:33:40 +0200

    Added scopeWithRole() to filter users according to their roles

--- src/Entrust/Traits/EntrustUserTrait.php
+++ src/Entrust/Traits/EntrustUserTrait.php
@@ -286,4 +286,18 @@ trait EntrustUserTrait
         }
     }
 
+    /**
+     *Filtering users according to their role 
+     *
+     *@param string $role
+     *@return users collection
+     */
+    public function scopeWithRole($query, $role)
+    {
+        return $query->whereHas('roles', function ($query) use ($role)
+        {
+            $query->where('name', $role);
+        });
+    }
+
 }
