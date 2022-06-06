1166fc3a9068370cc58755e0bc77a3a571001ec3
Nuno Cunha <nunorafael_6@hotmail.com>
2022-06-06 11:53:50 +0100

    test

--- src/Entrust/Traits/EntrustUserTrait.php
+++ src/Entrust/Traits/EntrustUserTrait.php
@@ -107,6 +107,7 @@ trait EntrustUserTrait
      */
     public function hasRole($name, $requireAll = false)
     {
+        var_dump('hasRole');
         if (is_array($name)) {
             foreach ($name as $roleName) {
                 $hasRole = $this->hasRole($roleName);
